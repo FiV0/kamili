d95ee29566c8ba02ffd0bb495854980d38379d82
FiV0 <finn.volkel@gmail.com>
2020-11-09 23:11:59 +0100

    Add cider-refresh commands

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,6 +2,8 @@
          (cider-clojure-cli-global-options . "-A:dev:test")
          (cider-default-cljs-repl . custom)
          (cider-custom-cljs-repl-init-form . "(do (user/go) (user/cljs-connect) (user/browse 8280))")
+         (cider-refresh-before-fn . "integrant.repl/suspend")
+         (cider-refresh-after-fn . "integrant.repl/resume")
          (eval . (progn
                    (make-variable-buffer-local 'cider-jack-in-nrepl-middlewares)
                    (add-to-list 'cider-jack-in-nrepl-middlewares "shadow.cljs.devtools.server.nrepl/middleware"))))))
