27249fa895250a50a7754b9ad4e77dd81439a888
FiV0 <finn.volkel@gmail.com>
2020-11-12 00:09:06 +0100

    Search page

--- src/io/kamili/view/search.cljs
+++ src/io/kamili/view/search.cljs
@@ -1,5 +1,25 @@
 (ns io.kamili.view.search
-  (:require [io.kamili.view :as view]))
+  (:require [io.kamili.view :as view]
+            [re-frame.core :as rf]
+            [reagent.core :as reagent]
+            [re-com.core :refer [input-text]])
+  (:require-macros [re-com.core :refer [handler-fn]]))
+
+(defn search-page []
+  (let [query (reagent/atom "")]
+    (fn []
+      [input-text
+       :model query
+       :on-change #(reset! query %)
+       :attr {:auto-focus false
+              :on-focus     (handler-fn (-> event .-target .select))
+              :on-key-press (handler-fn (case (.-which event)
+                                          13 (rf/dispatch [:navigate-to [:nav/results {:search @query}]])
+                                          nil))
+              :size 70
+              :type "text"
+              :placeholder "Search..."}
+       :change-on-blur? false])))
 
 (defmethod io.kamili.view/route-view :kamili.ui/search [_match]
-  [:div [:h1 "Search"]])
+  [search-page])
